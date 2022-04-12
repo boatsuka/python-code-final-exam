5edda9091115214f08a65238ccdb636c027a4bbc
boat.suka <boat.suka@gmail.com>
2022-04-12 13:00:09 +0700

    final code

---
+++ final-exam.py
@@ -0,0 +1,133 @@
+while True:
+    menber = int(input('กรุณาป้อนรหัสนักเรียน : '))
+    score = int(input('กรุณาป้อนคะแนน 0 - 100 : '))
+
+    if score >= 90:
+        print('คุณรหัส %d ได้เกรด A ', menber)
+        status = input('ต้องการคำนวณตัดเกรดใหม่หรือไม yes/no : ')
+    elif score >= 80:
+        print('คุณรหัส %d ได้เกรด B ', menber)
+        status = input('ต้องการคำนวณตัดเกรดใหม่หรือไม yes/no : ')
+    elif score >= 70:
+        print('คุณรหัส %d ได้เกรด C ', menber)
+        status = input('ต้องการคำนวณตัดเกรดใหม่หรือไม yes/no : ')
+    elif score >= 60:
+        print('คุณรหัส %d ได้เกรด D ', menber)
+        status = input('ต้องการคำนวณตัดเกรดใหม่หรือไม yes/no : ')
+    else:
+        print('คุณรหัส %d ได้เกรด F ', menber)
+        status = input('ต้องการคำนวณตัดเกรดใหม่หรือไม yes/no : ')
+
+    if status == "no":
+        break
+# *********************************************************************
+
+while input('ต้องการคำนวณตัดเกรดใหม่หรือไม yes/no : ') == "yes":
+    menber = int(input('กรุณาป้อนรหัสนักเรียน 6 หลัก : '))
+    score = int(input('กรุณาป้อนคะแนน 0 - 100 : '))
+
+    if score >= 90:
+        print('คุณรหัส %d ได้เกรด A ' % menber)
+    elif score >= 80:
+        print('คุณรหัส %d ได้เกรด B ' % menber)
+    elif score >= 70:
+        print('คุณรหัส %d ได้เกรด C ' % menber)
+    elif score >= 60:
+        print('คุณรหัส %d ได้เกรด D ' % menber)
+    else:
+        print('คุณรหัส %d ได้เกรด F ' % menber)
+
+# *********************************************************************
+status = None
+
+while status not in ("yes", "no"):
+    status = input("Enter yes or no : ")
+
+    if status == "yes":
+        menber = int(input('กรุณาป้อนรหัสนักเรียน 6 หลัก : '))
+        score = int(input('กรุณาป้อนคะแนน 0 - 100 : '))
+
+        if score >= 90:
+            print('คุณรหัส %d ได้เกรด A ' % menber)
+        elif score >= 80:
+            print('คุณรหัส %d ได้เกรด B ' % menber)
+        elif score >= 70:
+            print('คุณรหัส %d ได้เกรด C ' % menber)
+        elif score >= 60:
+            print('คุณรหัส %d ได้เกรด D ' % menber)
+        else:
+            print('คุณรหัส %d ได้เกรด F ' % menber)
+
+        status = None
+
+    elif status == 'no':
+        break
+
+    else:
+        print("Please enter yes or no.")
+
+# *********************************************************************
+
+status = ['yes', 'y']
+
+while input('ต้องการคำนวณตัดเกรดใหม่หรือไม yes/no : ').lower() in status :
+    try:
+        menber = int(input('กรุณาป้อนรหัสนักเรียน 6 หลัก : '))
+        score = int(input('กรุณาป้อนคะแนน 0 - 100 : '))
+
+        if score >= 90:
+                print('คุณรหัส %d ได้เกรด A ' % menber)
+        elif score >= 80:
+                print('คุณรหัส %d ได้เกรด B ' % menber)
+        elif score >= 70:
+                print('คุณรหัส %d ได้เกรด C ' % menber)
+        elif score >= 60:
+                print('คุณรหัส %d ได้เกรด D ' % menber)
+        else:
+                print('คุณรหัส %d ได้เกรด F ' % menber)
+    except ValueError:
+        print('กรุณาป้อนรหัสนักเรียนและคะแนน')
+
+# *********************************************************************
+
+status = ['yes', 'y']
+
+while input('ต้องการคำนวณตัดเกรดใหม่หรือไม yes/no : ').lower() in status :
+    try:
+        menber = int(input('กรุณาป้อนรหัสนักเรียน 6 หลัก : '))
+        score = int(input('กรุณาป้อนคะแนน 0 - 100 : '))
+
+        if score >= 90:
+                print('คุณรหัส %d ได้เกรด A ' % menber)
+        elif score >= 80:
+                print('คุณรหัส %d ได้เกรด B ' % menber)
+        elif score >= 70:
+                print('คุณรหัส %d ได้เกรด C ' % menber)
+        elif score >= 60:
+                print('คุณรหัส %d ได้เกรด D ' % menber)
+        else:
+                print('คุณรหัส %d ได้เกรด F ' % menber)
+    except ValueError:
+        print('กรุณาป้อนรหัสนักเรียนและคะแนน')
+
+# *********************************************************************
+
+status = ['yes', 'y']
+
+while input('ต้องการคำนวณตัดเกรดใหม่หรือไม yes/no : ').lower() in status:
+
+    menber = int(input('กรุณาป้อนรหัสนักเรียน 6 หลัก : '))
+    score = int(input('กรุณาป้อนคะแนน 0 - 100 : '))
+
+    if score >= 90:
+        print('คุณรหัส %d ได้เกรด A ' % menber)
+    elif score >= 80:
+        print('คุณรหัส %d ได้เกรด B ' % menber)
+    elif score >= 70:
+        print('คุณรหัส %d ได้เกรด C ' % menber)
+    elif score >= 60:
+        print('คุณรหัส %d ได้เกรด D ' % menber)
+    else:
+        print('คุณรหัส %d ได้เกรด F ' % menber)
+
+# *********************************************************************
